c79e1ab89e1bbeef90c25ad28d31082de749f931
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-13 22:32:55 +0200

    Update page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,21 @@
 	import { supabase } from '$lib/supabase';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col min-h-screen items-center justify-center w-full">
+	<form
+		method="POST"
+		class="flex flex-col gap-4 w-[400px] m-auto px-4 py-8 bg-white border-gray-900 border-2"
+	>
+		<div class="flex flex-col gap-2">
+			<label for="email">Email</label>
+			<input name="email" id="email" type="email" class="bg-gray-300 px-4 py-2" />
+		</div>
+		<div class="flex flex-col gap-2">
+			<label for="password">Password</label>
+			<input name="password" id="password" type="password" class="bg-gray-300 px-4 py-2" />
+		</div>
+		<button class="px-4 py-2 bg-gray-900 text-white w-fit m-auto">Sign up</button>
+	</form>
 
-<form
-	method="POST"
-	class="flex flex-col gap-4 max-w-md m-auto px-4 py-8 bg-white border-gray-900 border-2"
->
-	<div class="flex flex-col gap-2">
-		<label for="email">Email</label>
-		<input name="email" id="email" type="email" class="bg-gray-300 px-4 py-2" />
-	</div>
-	<div class="flex flex-col gap-2">
-		<label for="password">Password</label>
-		<input name="password" id="password" type="password" class="bg-gray-300 px-4 py-2" />
-	</div>
-	<button class="px-4 py-2 bg-gray-900 text-white w-fit m-auto">Sign up</button>
-</form>
-
-<p>Already register ? Please log in <a href="#" class="underline text-blue-500">here.</a></p>
+	<p>Already register ? Please log in <a href="#" class="underline text-blue-500">here.</a></p>
+</div>
