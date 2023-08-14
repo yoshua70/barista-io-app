15e7171cd79de8f64156eacdca0a2e255b37193d
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-14 13:33:39 +0200

    Define layout for user routes

---
+++ src/routes/user/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<div class="grid grid-cols-6 min-h-screen">
+	<div class="col-span-1 bg-gray-200 flex flex-col gap-4 p-4">
+		<p><a href="/user" class="no-underline text-gray-900 uppercase font-bold">Account</a></p>
+		<p><a href="/user/grids" class="no-underline text-gray-900 uppercase font-bold">Grids</a></p>
+	</div>
+	<div class="col-span-5 p-4">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/user/grids/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Barista.io - Grids</title>
+</svelte:head>
+
+<h1 class="text-3xl">Grids</h1>
