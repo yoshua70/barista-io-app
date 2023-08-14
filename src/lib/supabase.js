f11c5f914a507bab9bd19f6738fa1d87edd5908b
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-14 21:42:29 +0200

    Delete supabase client export file

--- src/lib/supabase.js
+++
@@ -1,4 +0,0 @@
-import { env } from "$env/dynamic/public";
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
