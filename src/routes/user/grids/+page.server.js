4c51a9774b21662db242d0481412f25083926a7d
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-14 22:23:05 +0200

    Fetch list of grids

--- src/routes/user/grids/+page.server.js
+++ src/routes/user/grids/+page.server.js
@@ -1,3 +1,5 @@
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import { fail } from '@sveltejs/kit';
 
 /**
@@ -10,13 +12,20 @@ export const actions = {
 
         const supabase = event.locals.supabase
 
+        const session = await event.locals.getSession()
+
+        if (!session) {
+            throw fail(403, {message: 'Unauthorized'})
+        }
+
         const {error} = await supabase
             .from('grid')
             .insert({
                 name: data.get('name'),
                 rows: data.get('rows'),
                 columns: data.get('columns'),
-                description: data.get('description')
+                description: data.get('description'),
+                user_id: session.user.id
             })
 
         if (!error) {
@@ -25,5 +34,21 @@ export const actions = {
 
         console.log(error)
         return fail(400, {message: error.message, incorrect: true})
+
     }
 }
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals }) {
+
+    console.log(await locals.getSession())
+
+    const {data, error} = await locals.supabase.from('grid').select(`name, rows, columns, description`)
+
+    console.log("Data", data)
+    console.log("Error", error)
+
+    return {
+      gridList: data
+    };
+  }
