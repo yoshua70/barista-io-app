8799d0c2b390045e2986a3a4d027aad45872b61b
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-14 21:01:51 +0200

    Create components to display the list of created grids

---
+++ src/routes/user/grids/components/GridList.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<div class="flex flex-col gap-8 rounded-md bg-white drop-shadow-xl p-8">
+	<div>
+		<h2 class="text-xl font-manrope">Active grids</h2>
+		<div class="flex gap-8 italic text-gray-500">
+			<p>The list of the grids you have created.</p>
+		</div>
+	</div>
+</div>
