5ee1b76de1939b18359d8bb11d7b099ca43a986f
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-16 22:16:46 +0200

    Display list of grids

--- src/routes/user/grids/components/GridList.svelte
+++ src/routes/user/grids/components/GridList.svelte
@@ -1,12 +1,28 @@
 <script>
+	import GridCard from './GridCard.svelte';
+
+	/**
+	 * @type []grid
+	 */
 	export let gridList;
 </script>
 
 <div class="flex flex-col gap-8 rounded-md bg-white drop-shadow-xl p-8">
-	<div>
+	<div class="flex flex-col gap-4">
 		<h2 class="text-xl font-manrope">Active grids</h2>
 		<div class="flex gap-8 italic text-gray-500">
 			<p>The list of the grids you have created.</p>
+		</div>
+		<div class="flex gap-4 flex-wrap">
+			{#each gridList as grid}
+				<GridCard
+					name={grid.name}
+					rows={grid.rows}
+					columns={grid.columns}
+					description={grid.description}
+					id={grid.id}
+				/>
+			{/each}
 		</div>
 	</div>
 </div>
