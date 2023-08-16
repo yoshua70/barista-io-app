cda6b351d836aa0f95a05cc7e3f1357197a9af0e
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-16 22:16:09 +0200

    Create component to display a single grid information

---
+++ src/routes/user/grids/components/GridCard.svelte
@@ -0,0 +1,44 @@
+<script>
+	/**
+	 * @type string
+	 */
+	export let id;
+	/**
+	 * @type string
+	 */
+	export let name;
+
+	/**
+	 * @type string
+	 */
+	export let rows;
+
+	/**
+	 * @type string
+	 */
+	export let columns;
+
+	/**
+	 * @type string | undefined
+	 */
+	export let description;
+</script>
+
+<div class="p-4 bg-white drop-shadow-md rounded-md flex flex-col gap-4 w-[300px]">
+	<h3 class="text-xl font-bold">{name}</h3>
+	<div>
+		<p>Columns: {columns}</p>
+		<p>Rows: {rows}</p>
+	</div>
+	<p>Description: {description ? description : 'nothing here...'}</p>
+	<a
+		href={`/user/grids/order/${id}`}
+		class="px-8 py-4 rounded-md bg-green-300 text-black w-full text-center font-poppins font-semibold hover:bg-green-200 transition-all duration-150 no-underline"
+		>Take order</a
+	>
+	<a
+		href={`/user/grids/edit/${id}`}
+		class="px-8 py-4 rounded-md bg-gray-300 text-black w-full text-center font-poppins font-semibold hover:bg-gray-200 transition-all duration-150 no-underline"
+		>Edit</a
+	>
+</div>
