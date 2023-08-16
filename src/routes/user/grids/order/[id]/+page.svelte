1b7987903d5f2e1d499243eb1172c9250a51021b
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-16 22:17:43 +0200

    Create order page for grid

---
+++ src/routes/user/grids/order/[id]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	const { grid } = data;
+	const currentGrid = grid[0];
+</script>
+
+<div class="flex gap-4 items-center">
+	<h1 class="font-manrope text-2xl font-bold">Take order</h1>
+	<div class="flex justify-between items-center bg-green-200 w-full rounded-md p-4">
+		<h1 class="font-manrope text-2xl font-bold">
+			{currentGrid.name}
+		</h1>
+		<a href={`/user/grids/edit/${currentGrid.id}`} class="no-underline text-gray-500">Edit</a>
+	</div>
+</div>
